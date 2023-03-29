b3800c7b1de9cfbef69e57b6d3b454ec576c7d8f
Harel Natan <harelnatan7@gmail.com>
2023-03-30 01:31:35 +0300

    added remove chat

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,6 +1,6 @@
 import { db } from '@/firebase'
 import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/outline'
-import { collection } from 'firebase/firestore'
+import { collection, deleteDoc, doc } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
@@ -26,13 +26,17 @@ function ChatRow({ id }: Props) {
     const [messages] = useCollection(
         collection(db, 'users', session?.user?.email!, 'chats', id, 'messages')
     )
-    
+
+    const removeChat = async () => {
+       await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id)) 
+       router.replace("/")
+    }
     return <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && 'bg-gray-700/50'}`}>
         <ChatBubbleLeftIcon className='h-5 w-5 ' />
         <p className='flex-1 hidden md:inline-flex truncate'>
             {messages?.docs[messages?.docs.length - 1]?.data().text || 'New Chat'}
         </p>
-        <TrashIcon className='h-5 w-5 text-gray-700 hover:text-red-700 transition-all' />
+        <TrashIcon onClick={removeChat} className='h-5 w-5 text-gray-700 hover:text-red-700 transition-all' />
     </Link>
 
 }
